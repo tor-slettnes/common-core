76ea6cfae32812ce73d47b9f05ddc749be5dd2a6
Tor Slettnes <tslettnes@picarro.com>
2024-06-20 00:46:04 -0700

    Create Python `wheel` file directly, without `sdist`

--- cmake/cpack_wheel.cmake
+++ cmake/cpack_wheel.cmake
@@ -15,6 +15,17 @@
 # message(STATUS "JSON TEXT (${_json_file_size} bytes, ${_json_file_time}): ${_json_text}")
 # message(STATUS "----- cpack_wheel end --------")
 
+function(find_include_dirs SOURCE_DIR TARGET_DIR OUT_VAR)
+  file(GLOB _include_dirs
+    RELATIVE "${CPACK_TEMPORARY_DIRECTORY}"
+    "${CPACK_TEMPORARY_DIRECTORY}/*${CPACK_PACKAGING_INSTALL_PREFIX}/${SOURCE_DIR}"
+  )
+
+  list(TRANSFORM _include_dirs REPLACE "^(.+)$" "\"\\1\" = \"${TARGET_DIR}\"")
+  list(JOIN _include_dirs "\n" _include_string)
+  set(${OUT_VAR} "${_include_string}" PARENT_SCOPE)
+endfunction()
+
 ### Generate `pyproject.toml` file
 set(PACKAGE "${CPACK_PACKAGE_NAME}")
 set(DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
@@ -23,29 +34,27 @@ set(HOMEPAGE_URL "${CPACK_PACKAGE_HOMEPAGE_URL}")
 
 ### Get author name and email from `${CPACK_PACKAGE_CONTACT}`
 if(CPACK_PACKAGE_CONTACT)
-  string(REGEX REPLACE "^([^<]*) <(.*)>" "\\1" AUTHOR_NAME "${CPACK_PACKAGE_CONTACT}")
-  string(REGEX REPLACE "^([^<]*) <(.*)>" "\\2" AUTHOR_EMAIL "${CPACK_PACKAGE_CONTACT}")
+  string(REGEX MATCH "^([^<]+) <(.*)>" _match "${CPACK_PACKAGE_CONTACT}")
+  set(AUTHOR_NAME "${CMAKE_MATCH_1}")
+  set(AUTHOR_EMAIL "${CMAKE_MATCH_2}")
 endif()
+
 
 ### Get Python package dependencies from `${CPACK_PYTHON_DEPENDENCIES}`
 list(TRANSFORM CPACK_PYTHON_DEPENDENCIES REPLACE "^(.+)$" "\"\\1\"")
 list(JOIN CPACK_PYTHON_DEPENDENCIES ", " DEPENDENCIES)
 
 ### Gather a list of Python folders to include
-file(GLOB _include_dirs
-  RELATIVE "${CPACK_TEMPORARY_DIRECTORY}"
-  "${CPACK_TEMPORARY_DIRECTORY}/*${CPACK_PACKAGING_INSTALL_PREFIX}/${CPACK_PYTHON_SOURCE_DIR}"
-  "${CPACK_TEMPORARY_DIRECTORY}/*${CPACK_PACKAGING_INSTALL_PREFIX}/${CPACK_SETTINGS_DIR}"
-)
+find_include_dirs("${CPACK_PYTHON_INSTALL_DIR}" "/" PYTHON_INCLUDE)
+find_include_dirs("${CPACK_SETTINGS_DIR}" "settings/" SETTINGS_INCLUDE)
 
-list(TRANSFORM _include_dirs REPLACE "^(.+)$" "\"\\1\"")
-list(JOIN _include_dirs ", " INCLUDE_DIRS)
-
+### Create the `pyproject.toml` file for the Python build backend
 configure_file(
   "${CMAKE_CURRENT_LIST_DIR}/python/pyproject.toml.in"
   "${CPACK_TEMPORARY_DIRECTORY}/pyproject.toml"
 )
 
+### Invoke the Python build
 find_package(Python3
   COMPONENTS Interpreter
 )
@@ -54,7 +63,7 @@ if(Python3_Interpreter_FOUND)
   message(STATUS "Invoking ${Python3_EXECUTABLE} -m build --outdir '${CPACK_PACKAGE_DIRECTORY}' .")
 
   execute_process(
-    COMMAND "${Python3_EXECUTABLE}" -m build --outdir "${CPACK_PACKAGE_DIRECTORY}" "."
+    COMMAND "${Python3_EXECUTABLE}" -m build --wheel --outdir "${CPACK_PACKAGE_DIRECTORY}" "."
     WORKING_DIRECTORY "${CPACK_TEMPORARY_DIRECTORY}"
   )
 else()
