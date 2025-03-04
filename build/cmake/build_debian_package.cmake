088609bb3ded69b206a86a187c0a43d5b90def63
Tor Slettnes <tslettnes@picarro.com>
2025-03-04 09:10:52 -0800

    FIX: Debian package dependencies were separated by `;` (CMake Lists); expand using `, ` separator

--- build/cmake/build_debian_package.cmake
+++ build/cmake/build_debian_package.cmake
@@ -29,23 +29,28 @@ endfunction()
 
 #===============================================================================
 ## @fn cc_cpack_add_group
-## @brief Wrapper for `cmake_add_component_group()` but with DEPENDS option.
-##
-## DEPENDS arguments should be additional component groups on which this depends.
+## @brief
+##   Wrapper for `cmake_add_component_group()` with added dependency options.
 
 function(cc_cpack_add_group GROUP)
   set(_options)
-  set(_singleargs DISPLAY_NAME DESCRIPTION)
-  set(_multiargs GROUP_DEPS DEB_DEPS)
+  set(_singleargs
+    DISPLAY_NAME               # One-line summary to describe this package group
+    DESCRIPTION                # Detailed description of this package group
+  )
+  set(_multiargs
+    GROUP_DEPS                 # Other cpack component groups on which we depend
+    PACKAGE_DEPS               # Other (full) package names on which we depend
+  )
   cmake_parse_arguments(arg "${_options}" "${_singleargs}" "${_multiargs}" ${ARGN})
 
   cpack_add_component_group("${GROUP}"
     DISPLAY_NAME "${DISPLAY_NAME}"
     DESCRIPTION "${DESCRIPTION}")
 
-  cc_cpack_add_group_dependencies("${GROUP}"
-    GROUP_DEPS ${arg_GROUP_DEPS}
-    DEB_DEPS ${arg_DEB_DEPS}
+  cc_cpack_set_group_dependencies("${GROUP}" DEPENDS
+    GROUPS ${arg_GROUP_DEPS}
+    PACKAGES ${arg_PACKAGE_DEPS}
   )
 
 endfunction()
@@ -53,25 +58,29 @@ endfunction()
 #===============================================================================
 ## @fn cc_cpack_add_group_dependencies
 
-function(cc_cpack_add_group_dependencies TARGET)
+function(cc_cpack_set_group_dependencies TARGET RELATIONSHIP)
   set(_options)
-  set(_singleargs RELATIONSHIP)
-  set(_multiargs GROUP_DEPS DEB_DEPS)
+  set(_singleargs)
+  set(_multiargs
+    GROUPS     # CPack component groups on which TARGET will have a relationship
+    PACKAGES   # Full package names on which TARGET will have a relationship
+  )
   cmake_parse_arguments(arg "${_options}" "${_singleargs}" "${_multiargs}" ${ARGN})
 
   string(TOUPPER "${TARGET}" upcase_target)
-  cc_get_value_or_default(relationship arg_RELATIONSHIP DEPENDS)
+  cc_get_value_or_default(relationship RELATIONSHIP DEPENDS)
 
-  list(TRANSFORM arg_GROUP_DEPS
+  list(TRANSFORM arg_GROUPS
     PREPEND "${PACKAGE_NAME_PREFIX}-"
     OUTPUT_VARIABLE ancestors
   )
 
-  list(APPEND ancestors ${arg_DEB_DEPS})
+  list(APPEND ancestors ${arg_PACKAGES})
+  list(JOIN ancestors ", " value)
 
   cc_cpack_config(
     "CPACK_DEBIAN_${upcase_target}_PACKAGE_${relationship}"
-    "${ancestors}"
+    "${value}"
   )
 endfunction()
 
@@ -86,8 +95,11 @@ endfunction()
 
 function(cc_cpack_debian_config VARIABLE_SUFFIX VALUE)
   set(_options APPEND)
-  set(_singleargs COMPONENT GROUP)
-  set(_multiargs VALUE)
+  set(_singleargs
+    COMPONENT # CPack component name, for when creating one package per component
+    GROUP     # CPack component group, for when creating one package per group
+  )
+  set(_multiargs)
   cmake_parse_arguments(arg "${_options}" "${_singleargs}" "${_multiargs}" ${ARGN})
 
   cc_get_cpack_debian_variable("${VARIABLE_SUFFIX}"
@@ -111,7 +123,13 @@ endfunction()
 
 function(cc_get_cpack_debian_variable VARIABLE_SUFFIX)
   set(_options)
-  set(_singleargs PREFIX SUFFIX COMPONENT GROUP OUTPUT_VARIABLE)
+  set(_singleargs
+    PREFIX         # CPackConfig variable prefix (normally `CPACK_DEBIAN`)
+    SUFFIX         # CPackConfig variable suffix
+    COMPONENT      # CPack component for when creating one package per component
+    GROUP          # CPack group for when creating one package per group
+    OUTPUT_VARIABLE # Variable in which to assign the resulting CPackConfig variable name
+  )
   set(_multiargs)
   cmake_parse_arguments(arg "${_options}" "${_singleargs}" "${_multiargs}" ${ARGN})
 
@@ -139,7 +157,14 @@ endfunction()
 
 function(cc_get_cpack_debian_grouping)
   set(_options)
-  set(_singleargs PREFIX SUFFIX GLUE COMPONENT GROUP OUTPUT_VARIABLE)
+  set(_singleargs
+    PREFIX         # CPackConfig variable prefix
+    SUFFIX         # CPackConfig variable suffix
+    GLUE           # Delimiter beween prefix/selection/suffix
+    COMPONENT      # CPack component for when creating one package per component
+    GROUP          # CPack group for when creating one package per group
+    OUTPUT_VARIABLE # Variable in which to store the resulting CPackConfig variable name
+  )
   set(_multiargs)
   cmake_parse_arguments(arg "${_options}" "${_singleargs}" "${_multiargs}" ${ARGN})
 
