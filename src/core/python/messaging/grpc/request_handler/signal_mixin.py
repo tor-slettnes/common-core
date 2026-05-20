2422e5649ba07eeef6d4e496a264494a9562ad84
Tor Slettnes <tslettnes@picarro.com>
2026-05-20 13:34:37 -0700

    Extract new `cc.grpc.request_handler.SignalMixIn` base class from `SignalRequestHandler`

---
+++ src/core/python/messaging/grpc/request_handler/signal_mixin.py
@@ -0,0 +1,82 @@
+'''
+signal_mixin.py - gRPC request handler mix-in with `Watch` method to feed back Signal messages
+'''
+
+__docformat__ = 'javadoc en'
+__author__= 'Tor Slettnes'
+
+### Standard Python modules
+from typing import Generator
+from queue  import Queue
+
+### Third-party modules
+import google.protobuf.message
+import grpc
+
+### Common Core modules
+from cc.protobuf.signal import SignalStore, Filter
+
+#===============================================================================
+# @class SignalMixIn
+
+class SignalMixIn:
+    '''
+    gRPC request handler mix-in with `Watch()` method to stream signals back
+    to client.
+    '''
+
+    signal_store = None
+
+    def __init__(self,
+                 signal_store   : SignalStore|None = None,
+                 max_queue_size : int|None = 256,
+                 ):
+        '''
+        param[in] signal_store
+           An instance of `protobuf.signal.SignalStore` from which
+           we receive signals.
+
+        param[in] max_queue_size
+           Max size of queue that will hold locally-emitted signals until
+           they are streamed back to the client.  None means unlimited.
+        '''
+
+        if signal_store:
+            self.signal_store = signal_store
+
+        else:
+            assert self.signal_store, (
+                'SignalRequestHandler subclass %s must define or pass in `signal_store`' % (
+                    type(self).__name__,
+                ))
+
+        self.max_queue_size = max_queue_size
+
+
+    def Watch(self,
+              request: Filter,
+              context: grpc.ServicerContext,
+              ) -> Generator[None, google.protobuf.message.Message, None]:
+        '''
+        Invoked by gRPC client to stream signal events back to client.
+
+        One invoked, we create a `Queue` instance which will capture
+        signals emitted locally on the server side, then yield messages
+        from this queue back to the client.
+        '''
+
+        queue = Queue(self.max_queue_size)
+        self.last_context = context
+
+        try:
+            for signal_name in self.signal_store.signal_names(request):
+                self.signal_store.connect_signal(signal_name, queue.put_nowait)
+
+            ### Add an empty message to indicate the end of the initial cache
+            queue.put_nowait(self.signal_store.signal_type())
+
+            while msg := queue.get():
+                yield msg
+
+        finally:
+            self.signal_store.disconnect_signal(signal_name)
